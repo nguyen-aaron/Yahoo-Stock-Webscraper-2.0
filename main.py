24a7430bfa8c655e77ee47707aa531c6c8aafd36
Aaron Nguyen <103400664+nguyen-aaron@users.noreply.github.com>
2022-10-20 07:28:58 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #import library
 import requests
 import time
 
 def dailyStocks():
-  startTime = time.time()
-  listLength = 5
+  startTime = time.time() #start timer
+  listLength = 5 #select the number of stocks returned per category
   def getTrendingStocks():
-    page = requests.get("https://finance.yahoo.com/trending-tickers")
-    soup = BeautifulSoup(page.content, "html.parser")
-    stockNameList = soup.find_all(attrs = {"aria-label" : "Name"})[0:listLength]
-    stockPriceList = soup.find_all(attrs = {"aria-label" : "Last Price"})[0:listLength]
-    stockChangeList = soup.find_all(attrs = {"aria-label" : "% Change"})[0:listLength]
-    print("Today's Trending:")
+    page = requests.get("https://finance.yahoo.com/trending-tickers") #get page
+    soup = BeautifulSoup(page.content, "html.parser") #scan for content
+    stockNameList = soup.find_all(attrs = {"aria-label" : "Name"})[0:listLength] #find first few indexes numbers of name of stocks
+    stockPriceList = soup.find_all(attrs = {"aria-label" : "Last Price"})[0:listLength] #find first few indexes of numbers of prices of stocks
+    stockChangeList = soup.find_all(attrs = {"aria-label" : "% Change"})[0:listLength] #find first few indexes numbers of percent change of stocks
+    print("Today's Trending:") #format the category header
     for x in range(0, listLength):
-      print(stockNameList[x].text, "-->", stockPriceList[x].text, "-->", stockChangeList[x].text)
-    print("")
+      print(stockNameList[x].text, "-->", stockPriceList[x].text, "-->", stockChangeList[x].text) #format output for all the information scraped prior
+    print("") #create blank line
 
   def getGainers():
     page = requests.get("https://finance.yahoo.com/gainers")
